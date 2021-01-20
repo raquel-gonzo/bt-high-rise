3e9e72ce95ff4f0dedd56aa57ed2978ece20d335
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2021-01-19 20:04:57 -0600

    minimum viable product.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import './App.css';
+import "./App.css";
 import { useState } from "react";
 import HighRise from "./components/HighRise";
 
 function App() {
-
   // declare the high-rise building using an array containing a number to represent each floor.
-  const floors = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  
-  
+  const floors = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1];
 
   return (
     <div className="App">
-      < HighRise floors={floors} />
+      <HighRise floors={floors} />
     </div>
   );
 }
---
+++ src/components/Elevator.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const Elevator = ({ id }) => {
+  const [currentFloor, setCurrentFloor] = useState(0);
+
+  function ascend(e) {
+    e.preventDefault();
+    setCurrentFloor(currentFloor + 1);
+  }
+
+  function descend(e) {
+    e.preventDefault();
+    if (currentFloor <= -1){
+      window.alert("You're on the lowest floor.")
+      return;
+    }
+    setCurrentFloor(currentFloor - 1)
+  }
+
+  return (
+    <div className="elevator">
+      <h1>Elevator {id}</h1>
+      <p>current floor: {currentFloor}</p>
+      <button onClick={ascend}>Up</button>
+      <button onClick={descend}>Down</button>
+    </div>
+  );
+};
+
+export default Elevator;
